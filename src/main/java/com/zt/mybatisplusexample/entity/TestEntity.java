181b6e8044e0141abe5f6d855d99e8c9cce920f6
tt <oun8634@dingtalk.com>
2022-12-08 16:01:18 +0800

    11

---
+++ src/main/java/com/zt/mybatisplusexample/entity/TestEntity.java
@@ -0,0 +1,12 @@
+package com.zt.mybatisplusexample.entity;
+
+/**
+ * @author : zt
+ * @version : V1.0
+ * @program : MyBatis-Plus-Example
+ * @date : Create in 2022/12/8 16:00
+ * @description :
+ */
+public class TestEntity {
+    private String name;
+}
